302e04a10894b6137892cd572d343d414f1b1904
Syureu <tor01237@gmail.com>
2020-07-18 16:57:18 +0900

    같은 방식으로 필드에 Autowired 가능, 그리고 Repository가 빈으로 등록 안된 상태에서 required 디폴트를 false로 주어 주입 안된채로 빈등록 하는 것도 똑같이 가능

--- 02-demospring51/src/main/java/me/syurue/demospring51/BookService.java
+++ 02-demospring51/src/main/java/me/syurue/demospring51/BookService.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookService {
 
-    BookRepository bookRepository;
-
     @Autowired(required = false)
-    public void setBookRepository(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
+    BookRepository bookRepository;
 }
