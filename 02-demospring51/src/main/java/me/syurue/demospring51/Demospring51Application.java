b496e244136a49838a5e52d92a2a56b79fe1f183
Syureu <tor01237@gmail.com>
2020-07-19 17:01:38 +0900

    ApplicationContextInitializer 인터페이스를 잘 수정하여 추가적인 빈을 하나 더 등록하는데, ApplicationRunner이고 간단하게 sout하는 러너를 추가해주는 원형 예제

--- 02-demospring51/src/main/java/me/syurue/demospring51/Demospring51Application.java
+++ 02-demospring51/src/main/java/me/syurue/demospring51/Demospring51Application.java
@@ -2,10 +2,14 @@ package me.syurue.demospring51;
 
 import me.syureu.out.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Demospring51Application {
@@ -16,7 +20,22 @@ public class Demospring51Application {
     public static void main(String[] args) {
         //SpringApplication.run(Demospring51Application.class, args);
         var app = new SpringApplication(Demospring51Application.class);
-        app.addInitializers((ApplicationContextInitializer<GenericApplicationContext>) ctx -> ctx.registerBean(MyService.class));
+        app.addInitializers((ApplicationContextInitializer<GenericApplicationContext>)
+                ctx -> {
+                    ctx.registerBean(MyService.class);
+                    ctx.registerBean(ApplicationRunner.class, new Supplier<ApplicationRunner>() {
+                        @Override
+                        public ApplicationRunner get() {
+                            return new ApplicationRunner() {
+                                @Override
+                                public void run(ApplicationArguments args) throws Exception {
+                                    System.out.println("Functional Bean Definition.");
+                                }
+                            };
+                        }
+                    });
+                }
+        );
         app.run(args);
     }
 
