3378e74242f125da1c6d513f1517c048d09e5324
Syureu <tor01237@gmail.com>
2020-07-20 14:58:40 +0900

    프로토 타입 스코프인 빈이 싱글톤 빈을 DI받으면 어떻게 될까? 프로토들은 다 다른 객체지만 필드로 들고있는 싱글은 같은 빈을 참조하고있다. 의도대로 동작한다.

--- 03-demospring527/src/main/java/me/syureu/demospring527/Proto.java
+++ 03-demospring527/src/main/java/me/syureu/demospring527/Proto.java
@@ -1,8 +1,12 @@
 package me.syureu.demospring527;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component @Scope("prototype")
 public class Proto {
+
+    @Autowired
+    Single single;
 }
