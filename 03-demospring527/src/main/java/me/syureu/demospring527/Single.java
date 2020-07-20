d68a0c0eb9536a3f16f1d8b291ad74538d4501fb
Syureu <tor01237@gmail.com>
2020-07-21 02:44:41 +0900

    6부 Envioronment - 1 initialize

--- 03-demospring527/src/main/java/me/syureu/demospring527/Single.java
+++ 03-demospring527/src/main/java/me/syureu/demospring527/Single.java
@@ -1,6 +1,5 @@
 package me.syureu.demospring527;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +7,9 @@ import org.springframework.stereotype.Component;
 public class Single {
 
     @Autowired
-    private ObjectProvider<Proto> proto;
+    Proto proto;
 
     public Proto getProto() {
-        return proto.getIfAvailable();
+        return proto;
     }
 }
