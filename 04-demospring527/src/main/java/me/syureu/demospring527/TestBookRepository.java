fab85581a2c24137e2974052509048649c0dcc8f
Syureu <tor01237@gmail.com>
2020-07-21 03:29:54 +0900

    Profile에 논리식 적용이 되어 이런 변태같은 프로파일 적용 조건도 만들 수는 있다...

--- 04-demospring527/src/main/java/me/syureu/demospring527/TestBookRepository.java
+++ 04-demospring527/src/main/java/me/syureu/demospring527/TestBookRepository.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Profile("test2")
+//@Profile("test2")
+@Profile("!test2 && test")
 public class TestBookRepository implements BookRepository {
 }
