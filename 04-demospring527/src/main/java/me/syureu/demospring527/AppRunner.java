272f9d04f76d7a21eeedf81b6c6e9e0f881d746f
Syureu <tor01237@gmail.com>
2020-07-21 03:26:44 +0900

    Configuration을 작성하면 컴포넌트가 오토스캔되지 않고 수동으로 빈 등록을 해주어야 하므로 불편하다. 컴포넌트 어노테이션이 프로파일을 작성해서 프로파일이 적용되게 빈등록을 할 수 있다.

--- 04-demospring527/src/main/java/me/syureu/demospring527/AppRunner.java
+++ 04-demospring527/src/main/java/me/syureu/demospring527/AppRunner.java
@@ -20,6 +20,9 @@ public class AppRunner implements ApplicationRunner {
     // Project Run Configuration에서 test라는 Profile을 적용하게 설정해준다면 에러없이 가동된다.
     // IDE가 이러한 옵션을 주지 않는 경우 VM option에 -Dspring.profiles.active="test" 와 같은 인수를 주면 된다.
 
+    // Configuration을 작성하면 오토스캔이 아닌 수동 빈 등록이라 귀찮다.
+    // 컴포넌트 어노테이션과 함께 프로파일 어노테이션을 작성해주면 프로파일 적용이 가능하다.
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment environment = ctx.getEnvironment();
--- 04-demospring527/src/main/java/me/syureu/demospring527/TestBookRepository.java
+++ 04-demospring527/src/main/java/me/syureu/demospring527/TestBookRepository.java
@@ -1,4 +1,9 @@
 package me.syureu.demospring527;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("test2")
 public class TestBookRepository implements BookRepository {
 }
