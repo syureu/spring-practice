9928b1c4fbbd2e08d1380a9a506f23bfb3a49c0c
Syureu <tor01237@gmail.com>
2020-07-20 14:51:59 +0900

    @Scope(prototype) 어노테이션을 통해 해당 빈의 스코프를 프로토타입으로 변경할 수 있다. 러너에서 Proto를 getBean할때마다 새로운 객체가 나오지만, Single은 싱글톤 스코프라 처음 만든 싱글톤객체를 계속 받는다.

--- 03-demospring527/src/main/java/me/syureu/demospring527/Proto.java
+++ 03-demospring527/src/main/java/me/syureu/demospring527/Proto.java
@@ -1,7 +1,8 @@
 package me.syureu.demospring527;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component @Scope("prototype")
 public class Proto {
 }
