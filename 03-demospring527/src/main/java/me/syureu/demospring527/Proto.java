6c4e69dc3eb92008d84fae96777d865e2994c43b
Syureu <tor01237@gmail.com>
2020-07-20 15:26:58 +0900

    proxyMode를 쓰지 않고 하는 방법으로는 ObjectProvider가 있는데 권장하지는 않는다. POJO에 스프링 코드가 녹아들게 되기 떄문이다.

--- 03-demospring527/src/main/java/me/syureu/demospring527/Proto.java
+++ 03-demospring527/src/main/java/me/syureu/demospring527/Proto.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-@Component @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Component @Scope(value = "prototype")
 public class Proto {
 
     @Autowired
