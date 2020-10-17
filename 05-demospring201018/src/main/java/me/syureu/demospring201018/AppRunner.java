69d960929bd570414d7a36d399918f03cd870096
DESKTOP-KNTBJAC\Administrator <tor01237@gmail.com>
2020-10-18 01:27:38 +0900

    이러한 방식으로 meesageSource를 사용 가능하다. 강의에서 기선님PC의 locale은 en_US로 기본 셋팅이 되어있으나 내 환경은 ko_KR이므로 default가 이미 ko_KR이다. 그러므로 다른 로케일을 쓸 수 있음을 보기위해 en_US의 프로퍼티를 작성하고, US의 로케일을 사용해봤다. 그리고 마찬가지로 applicationContext에서도 getMessage가 있을것이므로 같은 방법으로 사용가능하다. 다만 applicationContext에서 직접 getMessage를 쓰는 방식보다는 messageSource에서 getMessage를 쓰는게 같은 메서드를 쓰는거긴해도 코드에서 의도가 드러나기에 이게 좋은듯 하다.

--- 05-demospring201018/src/main/java/me/syureu/demospring201018/AppRunner.java
+++ 05-demospring201018/src/main/java/me/syureu/demospring201018/AppRunner.java
@@ -4,14 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
 
+import java.util.Locale;
+
+@Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
     ApplicationContext applicationContext;
 
+    @Autowired
+    MessageSource messageSource;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(Locale.getDefault());
+        System.out.println(messageSource.getMessage("greeting", new String[] {"Syureu"}, Locale.getDefault()));
+        System.out.println(messageSource.getMessage("greeting", new String[] {"Syureu"}, Locale.US));
+        System.out.println(messageSource.getMessage("greeting", new String[] {"Syureu"}, Locale.KOREA));
 
+        System.out.println(applicationContext.getMessage("greeting", new String[] {"Syureu"}, Locale.KOREA));
     }
 }
