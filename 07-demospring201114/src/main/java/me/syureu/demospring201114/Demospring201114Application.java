c08fffd2eded489d2a0e330ab543dfa7744842f0
DESKTOP-KNTBJAC\Administrator <tor01237@gmail.com>
2020-11-14 10:56:24 +0900

    9부 ResourceLoader initialize

---
+++ 07-demospring201114/src/main/java/me/syureu/demospring201114/Demospring201114Application.java
@@ -0,0 +1,13 @@
+package me.syureu.demospring201114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospring201114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demospring201114Application.class, args);
+    }
+
+}
---
+++ 07-demospring201114/src/test/java/me/syureu/demospring201114/Demospring201114ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.syureu.demospring201114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demospring201114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
