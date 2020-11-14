d465452442b87eb59abe0b04c97ab2817a38167a
DESKTOP-KNTBJAC\Administrator <tor01237@gmail.com>
2020-11-14 09:08:42 +0900

    @EnableAsync를 해줘서 이제 비동기적으로 이벤트 핸들러가 실행된다.

--- 06-demospring201018/src/main/java/me/syureu/demospring201018/Demospring201018Application.java
+++ 06-demospring201018/src/main/java/me/syureu/demospring201018/Demospring201018Application.java
@@ -2,8 +2,10 @@ package me.syureu.demospring201018;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Demospring201018Application {
 
     public static void main(String[] args) {
