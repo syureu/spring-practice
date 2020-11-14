a637ede81f0fd9782d8227675aec3de0b7885dbd
DESKTOP-KNTBJAC\Administrator <tor01237@gmail.com>
2020-11-14 09:04:05 +0900

    만약 이 핸들러들의 시퀀스를 결정해주고 싶다면 Order 어노테이션을 사용하면 된다. Ordered 를 임포트하여 순서를 지정해줄 수 있는데 HIGHEST_PRECEDENCE의 경우 -21억... 인트 최솟값으로 가고 LOWEST의 경우 21억4700만... 즉 인트 최댓값으로 간다. 즉 수가 작을 수록 우선순위가 높게 잡혀 실행됨을 알 수 있다. int형이므로 AnotherHandler의 핸들러 오더를 HIGHEST_PRECEDENCE + 2 로 해주면 앞전 실행에선 AnotherHandler의 핸들러가 먼저 실행되었지만 이제 우선순위를 조정해주었기 때문에 정해진 우선순위대로 AnotherHandler의 핸들러가 뒤에 실행됨을 확인 할 수 있다.

--- 06-demospring201018/src/main/java/me/syureu/demospring201018/AnotherHandler.java
+++ 06-demospring201018/src/main/java/me/syureu/demospring201018/AnotherHandler.java
@@ -1,12 +1,15 @@
 package me.syureu.demospring201018;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnotherHandler {
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE + 2)
     public void handle(MyEvent myEvent) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("data : " + myEvent.getData() + " and this is AnotherHandler");
--- 06-demospring201018/src/main/java/me/syureu/demospring201018/MyEventHandler.java
+++ 06-demospring201018/src/main/java/me/syureu/demospring201018/MyEventHandler.java
@@ -1,12 +1,15 @@
 package me.syureu.demospring201018;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyEventHandler {
 
     @EventListener
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("data : " + event.getData());
