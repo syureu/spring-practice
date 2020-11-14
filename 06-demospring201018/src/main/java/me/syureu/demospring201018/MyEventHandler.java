9e05639d0fb1b5aec1ea610a2b9b05c30c9d3128
DESKTOP-KNTBJAC\Administrator <tor01237@gmail.com>
2020-11-14 09:13:44 +0900

    스프링이 기본적으로 제공하는 이벤트가 몇가지 있다. ContextRefreshedEvent, ContextStartedEvent, ContextStoppedEvent, ContextClosedEvent, RequestHandledEvent 등등등.. 이 있다. 똑같이 이를 파람으로 받는 핸들러 작성해주면 된다. 부트에서는 이를 확장해서 더 다양한 이벤트를 제공해준다. 이건 부트가서 공부하자.

--- 06-demospring201018/src/main/java/me/syureu/demospring201018/MyEventHandler.java
+++ 06-demospring201018/src/main/java/me/syureu/demospring201018/MyEventHandler.java
@@ -1,5 +1,7 @@
 package me.syureu.demospring201018;
 
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -12,5 +14,19 @@ public class MyEventHandler {
     public void handle(MyEvent event) {
         System.out.println(Thread.currentThread().toString());
         System.out.println("data : " + event.getData());
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextRefreshedEvent contextRefreshedEvent) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextRefreshedEvent");
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextClosedEvent contextClosedEvent) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextClosedEvent");
     }
 }
