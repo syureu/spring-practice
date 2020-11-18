6ff4c14843811270fe59baafb224166a52600523
DESKTOP-KNTBJAC\Administrator <tor01237@gmail.com>
2020-11-18 14:04:56 +0900

    최근 스프링부트의 경우 벨리데이터를 직접 구현하기보다는 주입을 받아서 어노테이션 표기를 하여 사용한다. 강의 예제는 2.0.x 버전대로 보이는데 부트 2.3에서부터 크게 변화된 것이 validation starter를 더 이상 기본적으로 지원하지 않는다. 즉 boot starter web만 dependency에 등록한다면 validation 옵션이 제공되지 않는다. 커밋 기준 mvnrepository에서 제공하는 spring-boot-starter-web 버전은 2.4.0 이므로 validation 기능을 사용하고 싶으면 spring-boot-starter-validtion을 dependency에 등록해야 한다.

--- 07-demospring201114/src/main/java/me/syureu/demospring201114/AppRunner.java
+++ 07-demospring201114/src/main/java/me/syureu/demospring201114/AppRunner.java
@@ -1,23 +1,31 @@
 package me.syureu.demospring201114;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
 import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
+    @Autowired
+    Validator validator;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(validator.getClass());
+
         Event event = new Event();
-        EventValidator eventValidator = new EventValidator();
+        // EventValidator eventValidator = new EventValidator();
         Errors errors = new BeanPropertyBindingResult(event, "event");
 
-        eventValidator.validate(event, errors);
+        // eventValidator.validate(event, errors);
+        validator.validate(event, errors);
 
         System.out.println(errors.hasErrors());
 
